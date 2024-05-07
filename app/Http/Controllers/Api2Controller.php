bf136b4600af7594a9ba89a0847488a6270fc3f0
Adeveloper <ademnurhadi@gmail.com>
2024-05-08 02:28:47 +0700

    apiiii

--- app/Http/Controllers/Api2Controller.php
+++ app/Http/Controllers/Api2Controller.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sensordua;
 use Illuminate\Http\Request;
 
 class Api2Controller extends Controller
 {
     //
+    public function latest2()
+    {
+        $data = Sensordua::latest()->first();
+        return response()->json($data);
+    }
 }
