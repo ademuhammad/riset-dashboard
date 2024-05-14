03ef6059db48288c6daa2eded5d3856c8cdd9488
Adeveloper <ademnurhadi@gmail.com>
2024-05-14 17:57:18 +0700

    feat: update

--- app/Http/Controllers/GetdataController.php
+++ app/Http/Controllers/GetdataController.php
@@ -12,27 +12,41 @@ class GetdataController extends Controller
 {
 
 
+
     public function store(Request $request)
     {
         try {
-            $last_esp = Sensor::latest()->first();
-
+            // Get the current time
             $now = Carbon::now();
 
-            if (!$last_esp || $now->diffInSeconds(Carbon::parse($last_esp->created_at)) >= 10) {
-                $esp = Sensor::create([
-                    'value_suhu' => $request->value_suhu,
-                    'value_kelembaban' => $request->value_kelembaban,
-                    'value_pm25' => $request->value_pm25,
-                    'value_pm10' => $request->value_pm10,
-                    'value_co' => $request->value_co,
-                    'value_co2' => $request->value_co2,
-                ]);
-                Event::dispatch(new NewEspEvent($esp));
-                return response()->json($esp);
-            }
+            // Define the start and end time limits
+            $start_time = Carbon::createFromTime(7, 30);
+            $end_time = Carbon::createFromTime(16, 0);
 
-            return response()->json('success');
+            // Check if the current time is within the allowed range
+            if ($now->between($start_time, $end_time)) {
+                $last_esp = Sensor::latest()->first();
+
+                if (!$last_esp || $now->diffInSeconds(Carbon::parse($last_esp->created_at)) >= 10) {
+                    $esp = Sensor::create([
+                        'value_suhu' => $request->value_suhu,
+                        'value_kelembaban' => $request->value_kelembaban,
+                        'value_pm25' => $request->value_pm25,
+                        'value_pm10' => $request->value_pm10,
+                        'value_co' => $request->value_co,
+                        'value_co2' => $request->value_co2,
+                    ]);
+                    return response()->json($esp);
+                }
+
+                return response()->json('success');
+            } else {
+                // Return a response indicating the request was made outside the allowed time range
+                return response()->json([
+                    'code' => 400,
+                    'message' => 'Data can only be created between 07:30 and 16:00',
+                ]);
+            }
         } catch (\Exception $e) {
             return response()->json([
                 'code' => 400,
